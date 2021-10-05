742e7be179d109567943842cafaf34cb097ea904
Arm115 <ar.man.2003@list.ru>
2021-10-05 23:49:32 +0400

    02.css modules

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+
+
+const Header = () => {
+    return <header className='header'>
+    <img src='https://www.freelogoservices.com/blog/wp-content/uploads/transparent-logo.jpg' />
+  </header>
+}
+
+export default Header
---
+++ src/components/Profile.jsx
@@ -0,0 +1,26 @@
+
+
+const Profile = () => {
+    return <div className='content'>
+        <div>
+            <img width='1270' height='450' src='https://www.industrialempathy.com/img/remote/ZiClJf-1920w.jpg' />
+        </div>
+        <div>
+            ava + description
+        </div>
+        <div>
+            My posts
+            <div>
+                New posts
+            </div>
+            <div>
+                post 1
+            </div>
+            <div>
+                post 2
+            </div>
+        </div>
+    </div>
+}
+
+export default Profile
