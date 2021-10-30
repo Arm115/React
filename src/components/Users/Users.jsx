8cc40f347b6028fba4d10d070c9854ee50fc9cb2
Arm115 <ar.man.2003@list.ru>
2021-10-30 14:22:10 +0400

    24.life cycle,life cycle methods,componentdidmount

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -6,37 +6,13 @@ import { NavLink } from "react-router-dom";
 
 
 class UsersC extends React.Component {
-    constructor(props) {
-        super(props)
+    componentDidMount(){
         axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
             this.props.setUsers(response.data.items)
         });
     }
 
 
-    usersElements = this.props.users.map(u => <div key={u.id}>
-        <span>
-            <div>
-                <img src={u.photos.small != null ? u.photos.small : usersPhotoDefault} className={modules.images} />
-            </div>
-            <div>
-                {u.followed
-                    ? <button onClick={() => { this.props.unfollow(u.id) }}>Unfollow</button>
-                    : <button onClick={() => { this.props.follow(u.id) }}>Follow</button>
-                }
-            </div>
-        </span>
-        <span>
-            <span>
-                <div><NavLink to={'profile/' + u.id} activeClassName={modules.active}>{u.name}</NavLink></div>
-                <div>{u.status != null ? u.status : "Нет статуса"}</div>
-            </span>
-            <span>
-                {/* <div>{u.location.country}</div>
-                <div>{u.location.city}</div> */}
-            </span>
-        </span>
-    </div>)
 
     render() {
         return (
