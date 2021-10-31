5ac42c3f765137d2320411ae91e24107d8a7fc29
Arm115 <ar.man.2003@list.ru>
2021-10-31 21:09:51 +0400

    27.refactoring API component

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,68 +1,47 @@
-import * as axios from "axios";
-import React from "react";
 import modules from './Users.module.css'
 import usersPhotoDefault from './../../assets/images/usersDefaultPhoto.jpg'
 import { NavLink } from "react-router-dom";
 
+let Users = (props) => {
+    let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize)
 
-class Users extends React.Component {
-    componentDidMount() {
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
-            this.props.setUsers(response.data.items)
-        });
+    let pages = []
+    for (let i = 1; i <= pagesCount; i++) {
+        pages.push(i)
     }
-
-    onPageChanged = (currentPage) => {
-        this.props.setCurrentPage(currentPage)
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${currentPage}&count=${this.props.pageSize}`).then(response => {
-            this.props.setUsers(response.data.items)
-        });
-    }
-
-    render() {
-        let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize)
-
-        let pages = []
-        for (let i = 1; i <= pagesCount; i++) {
-            pages.push(i)
-        }
-
-        return (
+    return (
+        <div>
+            {pages.map(p => {
+                return <NavLink to={props.currentPage} className={props.currentPage === p && modules.pages} onClick={(e) => props.onPageChanged(p)}>{p}</NavLink>
+            })}
             <div>
-                {pages.map(p => {
-                    return <NavLink to={this.props.currentPage} className={this.props.currentPage === p && modules.pages} onClick={(e) => this.onPageChanged(p)}>{p}</NavLink>
-                })}
-                <div>
-                    {
-                        this.props.users.map(u => <div key={u.id}>
+                {
+                    props.users.map(u => <div key={u.id}>
+                        <span>
+                            <div>
+                                <img src={u.photos.small != null ? u.photos.small : usersPhotoDefault} className={modules.images} />
+                            </div>
+                            <div>
+                                {u.followed
+                                    ? <button onClick={() => { props.unfollow(u.id) }}>Unfollow</button>
+                                    : <button onClick={() => { props.follow(u.id) }}>Follow</button>
+                                }
+                            </div>
+                        </span>
+                        <span>
                             <span>
-                                <div>
-                                    <img src={u.photos.small != null ? u.photos.small : usersPhotoDefault} className={modules.images} />
-                                </div>
-                                <div>
-                                    {u.followed
-                                        ? <button onClick={() => { this.props.unfollow(u.id) }}>Unfollow</button>
-                                        : <button onClick={() => { this.props.follow(u.id) }}>Follow</button>
-                                    }
-                                </div>
+                                <div><NavLink to={'profile/' + u.id} activeClassName={modules.active}>{u.name}</NavLink></div>
+                                <div>{u.status != null ? u.status : "Нет статуса"}</div>
                             </span>
                             <span>
-                                <span>
-                                    <div><NavLink to={'profile/' + u.id} activeClassName={modules.active}>{u.name}</NavLink></div>
-                                    <div>{u.status != null ? u.status : "Нет статуса"}</div>
-                                </span>
-                                <span>
-                                    {/* <div>{u.location.country}</div>
+                                {/* <div>{u.location.country}</div>
                 <div>{u.location.city}</div> */}
-                                </span>
                             </span>
-                        </div>)}
-                </div>
+                        </span>
+                    </div>)}
             </div>
-        )
-
-    }
-
+        </div>
+    )
 }
 
 export default Users
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,8 +1,38 @@
 import React from "react"
 import { connect } from "react-redux"
-import { followAC, getCurrentPageAC, setCurrentPageAC, setUsersAC, unfollowAC } from "../../redux/users-reducer"
-import Users from "./Users"
+import { followAC, setCurrentPageAC, setUsersAC, unfollowAC } from "../../redux/users-reducer"
+import * as axios from "axios";
+import Users from "./Users";
 
+class UsersAPIContainer extends React.Component {
+    componentDidMount() {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
+            this.props.setUsers(response.data.items)
+            
+        });
+    }
+
+    onPageChanged = (currentPage) => {
+        this.props.setCurrentPage(currentPage)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${currentPage}&count=${this.props.pageSize}`).then(response => {
+            this.props.setUsers(response.data.items)
+        });
+    }
+
+    render() {
+
+        return <Users 
+            pageSize={this.props.pageSize} 
+            totalUsersCount={this.props.totalUsersCount}
+            currentPage={this.props.currentPage}
+            onPageChanged={this.onPageChanged}
+            follow={this.props.follow}
+            unfollow={this.props.unfollow}
+            users={this.props.users}/>
+
+    }
+
+}
 
 
 let mapStateToProps = (state) => {
@@ -33,6 +63,6 @@ let mapDispatchToProps = (dispatch) => {
     }
 }
 
-const UsersContainer = connect(mapStateToProps,mapDispatchToProps)(Users)
+const UsersContainer = connect(mapStateToProps,mapDispatchToProps)(UsersAPIContainer)
 
 export default UsersContainer
